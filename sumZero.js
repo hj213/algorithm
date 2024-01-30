d334392bb785d32ff2b2ce5bfca2fc1fd30a6d0c
hj213 <0213yhj@naver.com>
2024-01-31 00:41:43 +0900

    합0반환

---
+++ sumZero.js
@@ -0,0 +1,18 @@
+//배열에서 합이 0이 되는 첫번째 요소를 반환
+
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length -1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        } else if(sum > 0){
+            right--;
+        } else{
+            left++;
+        }
+    }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10])
--- vaildAnagram.js
+++ vaildAnagram.js
@@ -1,25 +1,30 @@
 //문자열1과 애너그램된 문자열2가 동일한지 비교
 
 function validAnagram(str1, str2){
-  let fqct1 = {};
-  let fqct2 = {};
-  
-  for(const char of str1){
-      fqct1[char] = (fqct1[char] || 0) + 1;
-  }
-  for(const char of str2){
-      fqct2[char] = (fqct2[char] || 0) + 1;
-  }
-  for(const key in fqct1){
-      if(!(key in fqct2)){
-          return false
-      }
-      if(fqct2[key] !== fqct1[key]){
-          return false
-          
-      }
-  }
+    if(str1.length !== str2.length){
+        return false
+    }
+    
+    let fqct1 = {};
+    let fqct2 = {};
+    
+    for(const char of str1){
+        fqct1[char] = (fqct1[char] || 0) + 1;
+    }
+    for(const char of str2){
+        fqct2[char] = (fqct2[char] || 0) + 1;
+    }
+    for(const key in fqct1){
+        if(!(key in fqct2)){
+            return false
+        }
+        if(fqct2[key] !== fqct1[key]){
+            return false
+            
+        }
+    }
   return true
 }
+
 
 validAnagram('aaz', 'zza')
