c597545118b45106493e2b8bfcdff1e317d44def
hj213 <0213yhj@naver.com>
2024-04-05 22:29:11 +0900

    그래프 순회

--- graph.js
+++ graph.js
@@ -22,11 +22,80 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    depthFirstRecursive(start){
+        let result = [];
+        let visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(n => {
+                if(!visited[n]) return dfs(n);
+            });
+        })(start);
+
+        return result;
+        
+    }
+    
+    depthFirstIterative(start){
+        let stack = [start];
+        let result = [];
+        let visited = {};
+        
+        visited[start] = true;
+        while(stack.length){
+            let currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(n => {
+                if(!visited[n]){
+                    visited[n] = true;
+                    stack.push(n);
+                }
+            })
+        }
+        
+        return result;
+    }
+    breadthFirst(start){
+        let queue = [start];
+        let result = [];
+        let visited = {};
+        visited[start] = true;
+
+        while(queue.length){
+            let currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(n => {
+                if(!visited[n]){
+                    visited[n] = true;
+                    queue.push(n);
+                } 
+            })
+        }
+        return result;
+    }
 }
 
 
 let g = new Graph();
 
-g.addVertex("jeju");
-g.addVertex("seoul");
-g.addVertex("busan");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdges("A","B");
+g.addEdges("A","C");
+g.addEdges("B","D");
+g.addEdges("C","E");
+g.addEdges("D","E");
+g.addEdges("D","F");
+g.addEdges("E","F");
+
